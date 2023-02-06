519d9757d667071e58c8ca85f2c654277f806bc5
saiabhiram <saiabhirampothini@gmail.com>
2023-02-06 21:21:02 +0530

    intial commit

---
+++ src/components/Log.js
@@ -0,0 +1,244 @@
+import React, { useState } from "react";
+import {
+  MDBContainer,
+  MDBTabs,
+  MDBTabsItem,
+  MDBTabsLink,
+  MDBTabsContent,
+  MDBTabsPane,
+  MDBBtn,
+  MDBIcon,
+  MDBInput,
+  MDBCheckbox,
+} from "mdb-react-ui-kit";
+
+function Log() {
+  const [justifyActive, setJustifyActive] = useState("tab1");
+
+  const handleJustifyClick = (value) => {
+    if (value === justifyActive) {
+      return;
+    }
+
+    setJustifyActive(value);
+  };
+
+  return (
+    <MDBContainer className="p-3 my-5 d-flex flex-column w-50">
+      <MDBTabs
+        pills
+        justify
+        className="mb-3 d-flex flex-row justify-content-between"
+      >
+        <MDBTabsItem>
+          <MDBTabsLink
+            onClick={() => handleJustifyClick("tab1")}
+            active={justifyActive === "tab1"}
+          >
+            Login
+          </MDBTabsLink>
+        </MDBTabsItem>
+        <MDBTabsItem>
+          <MDBTabsLink
+            onClick={() => handleJustifyClick("tab2")}
+            active={justifyActive === "tab2"}
+          >
+            Register
+          </MDBTabsLink>
+        </MDBTabsItem>
+      </MDBTabs>
+
+      <MDBTabsContent>
+        <MDBTabsPane show={justifyActive === "tab1"}>
+          {/* <div className="text-center mb-3">
+            <p>Sign in with:</p>
+
+            <div
+              className="d-flex justify-content-between mx-auto"
+              style={{ width: "40%" }}
+            >
+              <MDBBtn
+                tag="a"
+                color="none"
+                className="m-1"
+                style={{ color: "#1266f1" }}
+              >
+                <MDBIcon fab icon="facebook-f" size="sm" />
+              </MDBBtn>
+
+              <MDBBtn
+                tag="a"
+                color="none"
+                className="m-1"
+                style={{ color: "#1266f1" }}
+              >
+                <MDBIcon fab icon="twitter" size="sm" />
+              </MDBBtn>
+
+              <MDBBtn
+                tag="a"
+                color="none"
+                className="m-1"
+                style={{ color: "#1266f1" }}
+              >
+                <MDBIcon fab icon="google" size="sm" />
+              </MDBBtn>
+
+              <MDBBtn
+                tag="a"
+                color="none"
+                className="m-1"
+                style={{ color: "#1266f1" }}
+              >
+                <MDBIcon fab icon="github" size="sm" />
+              </MDBBtn>
+            </div> */}
+
+          {/* <p className="text-center mt-3">or:</p>
+          </div> */}
+          <h1 className="mb-4 mt-5">Please log in to continue</h1>
+
+          <MDBInput
+            wrapperClass="mb-4"
+            label="Email address"
+            id="form1"
+            type="email"
+          />
+          <MDBInput
+            wrapperClass="mb-4"
+            label="Password"
+            id="form2"
+            type="password"
+          />
+
+          <div className="d-flex justify-content-between mx-4 mb-4">
+            <MDBCheckbox
+              name="flexCheck"
+              value=""
+              id="flexCheckDefault"
+              label="Remember me"
+            />
+            <a href="!#">Forgot password?</a>
+          </div>
+
+          <MDBBtn className="mb-4 w-100">Sign in</MDBBtn>
+          {/* <p className="text-center">
+            Not a member? <a href="#!">Register</a>
+          </p> */}
+        </MDBTabsPane>
+
+        <MDBTabsPane show={justifyActive === "tab2"}>
+          {/* <div className="text-center mb-3">
+            <p>Sign un with:</p>
+
+            <div
+              className="d-flex justify-content-between mx-auto"
+              style={{ width: "40%" }}
+            >
+              <MDBBtn
+                tag="a"
+                color="none"
+                className="m-1"
+                style={{ color: "#1266f1" }}
+              >
+                <MDBIcon fab icon="facebook-f" size="sm" />
+              </MDBBtn>
+
+              <MDBBtn
+                tag="a"
+                color="none"
+                className="m-1"
+                style={{ color: "#1266f1" }}
+              >
+                <MDBIcon fab icon="twitter" size="sm" />
+              </MDBBtn>
+
+              <MDBBtn
+                tag="a"
+                color="none"
+                className="m-1"
+                style={{ color: "#1266f1" }}
+              >
+                <MDBIcon fab icon="google" size="sm" />
+              </MDBBtn>
+
+              <MDBBtn
+                tag="a"
+                color="none"
+                className="m-1"
+                style={{ color: "#1266f1" }}
+              >
+                <MDBIcon fab icon="github" size="sm" />
+              </MDBBtn>
+            </div>
+
+            <p className="text-center mt-3">or:</p>
+          </div> */}
+          <h1 className="mb-5">Welcome to Akova</h1>
+
+          <MDBInput wrapperClass="mb-4" label="Name" id="form1" type="text" />
+          <MDBInput
+            wrapperClass="mb-4"
+            label="Username"
+            id="form1"
+            type="text"
+          />
+          <MDBInput wrapperClass="mb-4" label="Email" id="form1" type="email" />
+          <MDBInput
+            wrapperClass="mb-4"
+            label="Password"
+            id="form1"
+            type="password"
+          />
+
+          <fieldset className="form-group">
+            <div className="row">
+              <legend className="col-form-label col-sm-2 pt-0">
+                Your profession:
+              </legend>
+              <div className="col-sm-10">
+                <div className="form-check">
+                  <input
+                    className="form-check-input"
+                    type="radio"
+                    name="gridRadios"
+                    id="gridRadios1"
+                    value="option1"
+                    // checked
+                  />
+                  <label className="form-check-label" htmlFor="gridRadios1">
+                    Student
+                  </label>
+                </div>
+                <div className="form-check">
+                  <input
+                    className="form-check-input"
+                    type="radio"
+                    name="gridRadios"
+                    id="gridRadios2"
+                    value="option2"
+                  />
+                  <label className="form-check-label" htmlFor="gridRadios2">
+                    Enterprenuer
+                  </label>
+                </div>
+              </div>
+            </div>
+          </fieldset>
+
+          <div className="d-flex justify-content-center mb-4">
+            <MDBCheckbox
+              name="flexCheck"
+              id="flexCheckDefault"
+              label="I have read and agree to the terms"
+            />
+          </div>
+
+          <MDBBtn className="mb-4 w-100">Sign up</MDBBtn>
+        </MDBTabsPane>
+      </MDBTabsContent>
+    </MDBContainer>
+  );
+}
+
+export default Log;
